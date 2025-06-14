c4c5908ed2754e779abc0d25fd144e3f6ea6ccfc
mjngxwnj <mjngxujian289@gmail.com>
2025-06-14 21:49:52 +0700

    add docx log

--- main.py
+++ main.py
@@ -43,3 +43,76 @@ def main():
 if __name__ == "__main__":
     main()
 
+"""
+[{'type': 'text', 'content': 'Cái này là ảnh con mèo nè\nCòn về văn bản hả. Mọi 
+người còn trông chờ gì hơn ngoài dòng này >>>>>>', 'meta': {'original_filename':
+ 'report.docx', 'original_extension': '.docx'}}, {'type': 'table', 'content':   
+Cái này là bảng nè  Label1                Label2     Label3 Label4
+0               Row1   Sdvsd  Dsvadf585623^$**^$%$  Dsvsdv xc    123
+1               Row2  Sdvsdv             524456354      Sdfsd    52r
+2                R\n  dsvsdv             Sdvsdvsdv       &*$@  dfbsf
+3               \n\n                                                , 'meta': {'
+original_filename': 'report.docx', 'original_extension': '.docx', 'source': 'tab
+le_1'}}, {'type': 'image', 'content': array([[[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+        ...,
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]],
+
+       [[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+        ...,
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]],
+
+       [[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+        ...,
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]],
+
+       ...,
+
+       [[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+        ...,
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]],
+
+       [[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+        ...,
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]],
+
+       [[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+        ...,
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]]], shape=(1430, 1430, 3), dtype=uint8), 'meta': {'original_fil
+ename': 'report.docx', 'original_extension': '.docx', 'source': 'image_1', 'size
+_pixels': (1430, 1430, 3)}}]
+docx after vectorization: 
+ [array([[0.05776227, 0.05776227, 0.        , 0.        , 0.05776227,
+        0.        , 0.        , 0.05776227, 0.05776227, 0.05776227,
+        0.        , 0.        , 0.05776227, 0.05776227, 0.        ,
+        0.05776227, 0.        , 0.05776227, 0.        , 0.        ],
+       [0.        , 0.        , 0.06931472, 0.06931472, 0.        ,
+        0.        , 0.06931472, 0.        , 0.        , 0.        ,
+        0.06931472, 0.06931472, 0.        , 0.        , 0.06931472,
+        0.        , 0.06931472, 0.        , 0.        , 0.06931472]]), array([],
+ shape=(4, 0), dtype=float64), array([0., 0., 0., ..., 0., 0., 0.], shape=(20449
+00,))]
+"""
